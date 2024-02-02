6c3ee337855e0ebab4a5b38a8bdc166b255799a3
Raza <128757716+Razadan1@users.noreply.github.com>
2024-02-02 15:29:49 -0800

    Update the percentage

--- main.h
+++ main.h
@@ -22,7 +22,7 @@ typedef struct convert conver_t;
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_char(va_list args);
-int print_per(va_list args);
+int print_percent(__attribute__((unused)) va_list args);
 int print_string(va_list args);
 int print_int(va_list args);
 
